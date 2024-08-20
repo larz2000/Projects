6abe426efbbbb7a9de5642592479bba8a661126b
larz2000 <larrythegreat.ls@gmail.com>
2024-08-20 13:41:39 +0800

    Add files via upload
    LabExer2.2

--- Program.cs
+++ Program.cs
@@ -4,41 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LabExer2
+namespace ComputerAverageApp
 {
-    internal class DataTypesProgram
+    internal class ComputerAverageProgram
     {
         static void Main(string[] args)
         {
-            int numOfApple,convToInt;//declare int variables
-            double priceOfApple,totalValue;// declare double variables
-            Console.Write("Enter the pieces of apple: ");//ask user
-            numOfApple = Convert.ToInt32(Console.ReadLine());//input value/s
-            
-            if (numOfApple>0) {//if the user input 0 then the program won't execute otherwise it will execute
-                Console.Write("Enter the price of each apple: ");//ask user
-                priceOfApple = Convert.ToDouble(Console.ReadLine());//input values
-                totalValue = priceOfApple * numOfApple;//multiply number of apple to each price
-                convToInt = (int)totalValue;//convert double to int
-
-                if (numOfApple > 1){
-                    Console.WriteLine("The total price of " + numOfApple + " apples is: " + totalValue);//output
-                    Console.WriteLine("--------------------------------------------");
-                    Console.WriteLine("The value of original price is:  " + totalValue);//output
-                    Console.WriteLine("The value of converted price is: "+convToInt);//output
-                    Console.WriteLine("--------------------------------------------");
-                }
-                else {
-                    Console.WriteLine("The total price of "+numOfApple+" apple is: "+totalValue);//output
-                    Console.WriteLine("--------------------------------------------");
-                    Console.WriteLine("The value of original price is:  " + totalValue);//output
-                    Console.WriteLine("The value of converted price is: " + convToInt);//output
-                    Console.WriteLine("--------------------------------------------");
-                }
-
-            }else{
-                Console.WriteLine("YOUR INPUT MUST BE GREATER THAN 0!");
+            double sumOfGrades=0;//initialize
+            Console.Write("Enter student's name: ");//ask user
+            string name = Console.ReadLine();//input
+            Console.Write("Enter the number of subject/s: ");//ask user
+            int numSubs = Convert.ToInt32(Console.ReadLine());//input
+            Console.WriteLine("Enter grade for each subject/s: ");//ask user
+            for (int i=0;i<numSubs;i++){//loop input by the number of subjects
+                sumOfGrades = sumOfGrades + Convert.ToDouble(Console.ReadLine());
             }
+            double ave = sumOfGrades/numSubs;//divide the sum of grades to the number of subjects to get the average
+            double rounded = Math.Round(ave);//round the average 
+            Console.WriteLine("The average grade of "+name+" is: "+ave);//output
+            Console.WriteLine("The final grade rounded off is: "+rounded);//output
         }
     }
 }
