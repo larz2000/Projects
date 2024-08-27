ae2e928df452380cb6b07a3cb7ce47ad3632a654
larz2000 <larrythegreat.ls@gmail.com>
2024-08-27 08:18:19 +0800

    Add files via upload

---
+++ StudentGradeApplication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double average;
+            Console.WriteLine("Student Grade Application");
+            Console.Write("\nName:   ");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nSubjects    Grades");
+            Console.Write("English :   ");
+            double english = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Math    :   ");
+            double math = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Science :   ");
+            double science = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Filipino:   ");
+            double filipino = Convert.ToDouble(Console.ReadLine());
+            Console.Write("History :   ");
+            double history = Convert.ToDouble(Console.ReadLine());
+
+            average = (english + math + science + filipino + history)/5;
+
+            string res = (average >= 75 ) ? "The student passed." : "The student failed.";
+
+            Console.WriteLine("\n"+res);
+            Console.WriteLine("The general average of " + name + " is: " + average+"\n");
+
+
+        }
+    }
+}
